6d9527527ee01a7daa7161d939f47845aec98fd7
Prattinger Michael <atpramic@einstein.local>
2016-10-07 12:35:46 +0200

    wip

---
+++ js/login.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    $(".form").find("input, textarea").on("keyup blur focus", function (e) {
+        var $this = $(this);
+        var label = $this.prev("label");
+
+        if (e.type === 'keyup') {
+            if ($this.val() === '') {
+                label.removeClass('active highlight');
+            } else {
+                label.addClass('active highlight');
+            }
+        } else if (e.type === 'blur') {
+            if ($this.val() === '') {
+                label.removeClass('active highlight');
+            } else {
+                label.removeClass('highlight');
+            }
+        } else if (e.type === 'focus') {
+            if ($this.val() === '') {
+                label.removeClass('highlight');
+            }
+            else if ($this.val() !== '') {
+                label.addClass('highlight');
+            }
+        }
+    });
+
+});
